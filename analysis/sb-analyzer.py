1f279137eefaba54720df4c8ede27a1c13189b18
Lukas Galke <git@lpag.de>
2016-06-28 12:25:25 +0200

    pre pilot results analysis

--- analysis/sb-analyzer.py
+++ analysis/sb-analyzer.py
@@ -6,52 +6,65 @@ import argparse
 import pandas as pd
 from scipy import stats
 
+def is_significant(pvalue, alpha=0.05):
+    return pvalue < alpha
+
+def dagostino_or_shapiro(data):
+    n = len(data)
+    test = stats.normaltest if n >= 20 else stats.shapiro
+    result = test(data)
+    return result
+
 
 def analyze_efficiency(df, verbose=0):
     nav_methods = df.groupby('navigation')
     burger_df, swipe_df = nav_methods.get_group('burger'), nav_methods.get_group('swipe')
-
+    print("%d BURGERS vs %d SWIPERS" % (len(burger_df.groupby('pid')), len(swipe_df.groupby("pid"))))
+    # print("=" * 78)
+    print("[BURGER]" + "=" * 70)
     burger = burger_df["time_ms"]
-    burger_shapiro = stats.shapiro(burger)
-    burger_normality = burger_shapiro[1] < 0.05
-    print("Checking Burger for normal distribution:", burger_normality, burger_shapiro)
+    normality_test = dagostino_or_shapiro(burger)
+    burger_isnorm = is_significant(normality_test[1])
+    print("Normality>", normality_test, burger_isnorm)
 
-
-    burger_tasks = [group["time_ms"] for _, group in burger_df.groupby("Tid")]
+    burger_tasks = [group["time_ms"] for _, group in burger_df.groupby("tid")]
     # alternatives: repeated measures anova, friedmann test
-    print("[BURGER] Kruskal-Wallis H-Test: ", stats.kruskal(*burger_tasks))
-    print("[BURGER] Friedmann Chi Square: ", stats.friedmanchisquare(*burger_tasks))
+    print(stats.kruskal(*burger_tasks))
+    print(stats.friedmanchisquare(*burger_tasks))
     # FIXME do these tests work as expected? (they report low p values but the data is all the same)
 
 
     if verbose:
-        print("Burger Navigation",burger.describe(), sep="\n")
+        print("[BURGER] Description",burger.describe(), sep="\n")
 
-    for tid, swipe_task_df in swipe_df.groupby("Tid"):
+    for tid, swipe_task_df in swipe_df.groupby("tid"):
+        print("=" * 78)
         swipe_task = swipe_task_df["time_ms"]
+        d = swipe_task_df["distance"].abs().mean()
         if verbose:
-            print("Swipe Navigation, Task %d" %tid, swipe_task.describe(), sep="\n")
-        swipe_task_shapiro = stats.shapiro(swipe_task)
-        swipe_task_normality = swipe_task_shapiro[1] < 0.05
-        print ("Checking Task", tid, "for normal distribution:", swipe_task_normality, swipe_task_shapiro)
+            print("[SWIPE: Task %d(d=%.1f)] Description" % (tid, d), swipe_task.describe(), sep="\n")
+        # Dagostino Pearson if possible, else shapiro
+        normality_test = dagostino_or_shapiro(swipe_task)
+        swipe_task_isnorm = is_significant(normality_test[1])
+        print("[SWIPE: Task %d(d=%.1f)] Normality" % (tid, d), normality_test, swipe_task_isnorm, sep="\n")
 
         result = stats.ttest_ind(swipe_task, burger, equal_var=False)\
-            if (burger_normality and swipe_task_normality) else\
+            if (burger_isnorm and swipe_task_isnorm) else\
             stats.mannwhitneyu(swipe_task, burger)
-        print(tid, ":", result, result[1] < 0.05)
+        print("[SWIPE: Task %d(d=%.1f)]" % (tid, d), result, is_significant(result[1]))
 
 
 def main():
     """ Main Function """
     parser = argparse.ArgumentParser()
-    parser.add_argument("file", nargs='?',type=argparse.FileType('r'), default="data-format.csv")
-    parser.add_argument("-v", "--verbose", action="count", default=0)
+    parser.add_argument("file", nargs='?',type=argparse.FileType('r'), default="prepilot.csv")
+    parser.add_argument("-v", "--verbose", action="count", default=1)
     args = parser.parse_args()
     df = pd.read_csv(args.file)
 
-    # assert that we have the same number of samples for each Pid, Tid per navigation method, ...
+    # assert that we have the same number of samples for each pid, tid per navigation method, ...
     check = {str(field) : len(pd.unique([len(group) for _, group in df.groupby(field)])) == 1
-             for field in ["Pid", ["navigation","Tid"]]}
+             for field in ["pid", ["navigation","tid"]]}
     print("Dataset Validation:", check)
     analyze_efficiency(df, verbose=args.verbose)
 
